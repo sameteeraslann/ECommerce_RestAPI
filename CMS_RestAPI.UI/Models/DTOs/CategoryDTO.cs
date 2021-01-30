4dd3871e47501254fe72146672747b325e573e87
Samet <samettt18@hotmail.com.tr>
2021-01-30 21:54:04 +0300

    CategoryDTO Eklendi

---
+++ CMS_RestAPI.UI/Models/DTOs/CategoryDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMS_RestAPI.UI.Models.DTOs
+{
+    public class CategoryDTO
+    {
+        public string Name { get; set; }
+        public string Slug { get; set; }
+    }
+}

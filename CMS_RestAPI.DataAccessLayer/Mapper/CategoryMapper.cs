e982222ca7a05283fc149006ffce24423eead169
Samet <samettt18@hotmail.com.tr>
2021-01-30 21:50:08 +0300

    AutoMapper Eklendi

---
+++ CMS_RestAPI.DataAccessLayer/Mapper/CategoryMapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using CMS_RestAPI.EntityLayer.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMS_RestAPI.DataAccessLayer.Mapper
+{
+    public class CategoryMapper:Profile
+    {
+        public CategoryMapper() => CreateMap<Category, CategoryDTO>().ReverseMap();
+    }
+}

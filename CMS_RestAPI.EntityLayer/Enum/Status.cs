8ce8d79953956e45916b7d0fed9503f40b5a2f6c
Samet <samettt18@hotmail.com.tr>
2021-01-30 16:26:09 +0300

    Enum klasörü açıldı

---
+++ CMS_RestAPI.EntityLayer/Enum/Status.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMS_RestAPI.EntityLayer.Enum
+{
+    public enum Status
+    {
+        Active = 1, Modified =2 , Passive =3
+    }
+}

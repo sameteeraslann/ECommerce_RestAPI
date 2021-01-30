0e1d907ea0602b12d3576f67d56c5de582bd4404
Samet <samettt18@hotmail.com.tr>
2021-01-30 19:01:11 +0300

    Context işlemi yapıldı

---
+++ CMS_RestAPI.DataAccessLayer/Context/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using CMS_RestAPI.EntityLayer.Entities.Concrete;
+using CMS_RestAPI.MappingLayer.Mapping.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMS_RestAPI.DataAccessLayer.Context
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)   { }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Page>  Pages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AppUserMap());
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new ProductMap());
+            modelBuilder.ApplyConfiguration(new PageMap());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

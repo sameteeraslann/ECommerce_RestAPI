11b1fb14605fd799cbfd2c3e31c711c2711678b9
Samet <samettt18@hotmail.com.tr>
2021-01-30 22:15:59 +0300

    AppUserMapper Eklendi

---
+++ CMS_RestAPI.UI/Mapper/AppUserMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CMS_RestAPI.EntityLayer.Entities.Concrete;
+using CMS_RestAPI.UI.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMS_RestAPI.UI.Mapper
+{
+    public class AppUserMapper :Profile
+    {
+        public AppUserMapper() => CreateMap<AppUser, AppUserDTO>().ReverseMap();
+        {
+
+        }
+    }
+}

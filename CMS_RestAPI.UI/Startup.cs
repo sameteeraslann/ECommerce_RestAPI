42202391b5b3390681c1be26208570ef2a06355e
Samet <samettt18@hotmail.com.tr>
2021-01-30 20:20:38 +0300

    Database Bağlantıları Kuruldu.

--- CMS_RestAPI.UI/Startup.cs
+++ CMS_RestAPI.UI/Startup.cs
@@ -1,6 +1,8 @@
+using CMS_RestAPI.DataAccessLayer.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,8 @@ namespace CMS_RestAPI.UI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnections")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

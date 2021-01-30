da029b4428c6f09a2ed40b85a414fc80875ce9b9
Samet <samettt18@hotmail.com.tr>
2021-01-30 22:14:53 +0300

    AppUserDTO Eklendi

---
+++ CMS_RestAPI.UI/Models/DTOs/AppUserDTO.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMS_RestAPI.UI.Models.DTOs
+{
+    public class AppUserDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public SecurityToken Token { get; set; }
+    }
+}

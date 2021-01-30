f3dbe1e57dacdde5689e23dafd4bd5e27bacc6ff
Samet <samettt18@hotmail.com.tr>
2021-01-30 16:29:47 +0300

    Concrete-Interface açıldı

---
+++ CMS_RestAPI.EntityLayer/Entities/Interface/IBaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using CMS_RestAPI.EntityLayer.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMS_RestAPI.EntityLayer.Entities.Interface
+{
+    public interface IBaseEntity
+    {
+        DateTime CreateDate { get; set; }
+        DateTime? UpdateDate { get; set; }
+        DateTime? DeleteDate { get; set; }
+        
+        Status Status { get; set; }
+    }
+}
